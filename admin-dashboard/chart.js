85fb3e2877c7a98eae15167749520a20352b7a55
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-29 05:21:08 +0300

    add charts

---
+++ admin-dashboard/chart.js
@@ -0,0 +1,49 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+const earn = document.getElementById('earning').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Instagram', 'Youtube', 'Amazon'],
+        datasets: [{
+            label: 'Traffic Source',
+            data: [1200, 1900, 3000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+const earning = new Chart(earn, {
+    type: 'bar',
+    data: {
+        labels: ['Janauary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [1200, 1900, 3000, 5000, 2000, 3400,1200, 1090, 3250, 2405, 2800, 3600],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
